9a2afd0c3659fd63cc717a7997c41fd8adbc817a
Artem Astafyev <AAstafyev@kazzinc.com>
2019-11-24 23:27:37 +0600

    добавил скрипт открывания/закрывания меню

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,5 @@
+console.log('hi there');
+
 var mobile = function() {
   setPageWrapper("320px", 0);
 };
@@ -13,4 +15,28 @@ var desktop = function() {
 var setPageWrapper = function(width, margin){
   var pageWrapper = document.querySelector(".page-wrapper");
   pageWrapper.classList.add("page-wrapper--mobile");
+}
+
+window.onload = function(e){
+  var mainNav = document.querySelector(".main-nav");
+  var bntOpen = document.querySelector(".main-nav__menu-button");
+  var bntClose = document.querySelector(".main-nav__cross-button");
+
+  mainNav.classList.remove("main-nav--opened");
+  mainNav.classList.add("main-nav--closed");
+  bntOpen.classList.remove("visually-hidden");
+
+  bntOpen.addEventListener("click", function(){
+    mainNav.classList.add("main-nav--opened");
+    mainNav.classList.remove("main-nav--closed");
+    bntOpen.classList.add("visually-hidden");
+    bntClose.classList.remove("visually-hidden");
+  });
+
+  bntClose.addEventListener("click", function(){
+    mainNav.classList.add("main-nav--closed");
+    mainNav.classList.remove("main-nav--opened");
+    bntOpen.classList.remove("visually-hidden");
+    bntClose.classList.add("visually-hidden");
+  });
 }
