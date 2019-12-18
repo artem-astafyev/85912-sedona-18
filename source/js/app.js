48ee038a72f2774d2e21fe6a7edad7d4fabae113
Artem Astafyev <AAstafyev@kazzinc.com>
2019-12-18 21:30:02 +0600

    добавил мобильную версию окон

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,4 @@
-const toggleMenu = function(e){
+const toggleMenu = function (e) {
   const mainNav = document.querySelector(".main-nav");
   const bntOpen = document.querySelector(".main-nav__menu-button");
   const bntClose = document.querySelector(".main-nav__cross-button");
@@ -7,18 +7,30 @@ const toggleMenu = function(e){
   mainNav.classList.add("main-nav--closed");
   bntOpen.classList.remove("visually-hidden");
 
-  bntOpen.addEventListener("click", function(){
+  bntOpen.addEventListener("click", function () {
     mainNav.classList.add("main-nav--opened");
     mainNav.classList.remove("main-nav--closed");
     bntOpen.classList.add("visually-hidden");
     bntClose.classList.remove("visually-hidden");
   });
 
-  bntClose.addEventListener("click", function(){
+  bntClose.addEventListener("click", function () {
     mainNav.classList.add("main-nav--closed");
     mainNav.classList.remove("main-nav--opened");
     bntOpen.classList.remove("visually-hidden");
     bntClose.classList.add("visually-hidden");
+  });
+
+  const successPopup = document.querySelector(".popup--success");
+  const successButton = document.getElementById("successButton");
+  successButton.addEventListener("click", function () {
+    successPopup.classList.add("display-none");
+  });
+
+  const failPopup = document.querySelector(".popup--fail");
+  const failButton = document.getElementById("failButton");
+  failButton.addEventListener("click", function () {
+    failPopup.classList.add("display-none");
   });
 }
 
